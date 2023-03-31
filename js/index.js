71c51ae70bd993baa71c952a6e3fa8a73fb80c8a
“IamKabir” <t2kaah00@students.oamk.fi>
2023-03-31 23:55:44 +0300

    change /

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const BACKEND_ROOT_URL = 'https://todo-backend-wdc8.onrender.com/';
+const BACKEND_ROOT_URL = 'https://todo-backend-wdc8.onrender.com';
 import { Todos } from "./class/Todos.js";
 const todos = new Todos(BACKEND_ROOT_URL);
 const list = document.querySelector('#todolist');
